3ffade4057066f55d4369d84a48855eea00d090b
Amir Goren <amir.goren@agritask.com>
2020-09-17 21:53:00 +0300

    add build process with webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+    mode,
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
