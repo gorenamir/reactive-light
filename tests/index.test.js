928cc21136330c9d9b82bf8b1754b8e5dcca864e
Amir Goren <amir.goren@agritask.com>
2020-09-18 14:40:44 +0300

    add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,40 @@
 import { ref, computed, watch, watchEffect } from '../src/index';
 
-test('counter is equal to 0', () => {
+test('computed functionality works', () => {
     const counter = ref(0);
+    const twoTimesCounter = computed(() => counter.value * 2);
+
     expect(counter.value).toBe(0);
+    expect(twoTimesCounter.value).toBe(0);
+
+    counter.value++;
+
+    expect(counter.value).toBe(1);
+    expect(twoTimesCounter.value).toBe(2);
 });
 
+test('watch functionality works', () => {
+    const msg = ref('hello, world!');
+    watch(
+        () => msg.value,
+        (newVal, oldVal) => {
+            expect(newVal).toBe('changed!');
+            expect(oldVal).toBe('hello, world!');
+        }
+    );
+    msg.value = 'changed!';
+});
+
+test('watchEffect works', () => {
+    let msg = ref('hello, world!');
+    let numOfInvocations = 0;
+    watchEffect(() => {
+        let rawMsg = msg.value;
+        numOfInvocations++;
+    });
+    msg.value += '!';
+    msg.value += '!';
+    msg.value += '!';
+
+    expect(numOfInvocations).toBe(4);
+});
