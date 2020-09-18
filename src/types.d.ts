dab0ec987e804d59514fa029a844761e4c7c554b
Amir Goren <amir.goren@agritask.com>
2020-09-18 12:00:14 +0300

    add typescript support

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,17 @@
-export {
-ref,
-computed,
-watch,
-watchEffect
-} from './index';
+declare class Ref<T = any> {
+    value: T;
+}
+
+declare function ref<T = any>(value: T): Ref<T>
+
+declare class Computed<T = any> {
+    readonly value: T;
+}
+
+declare function computed<T = any>(cb: () => T): Computed<T>
+
+declare function watchEffect(cb: () => void): void
+
+declare function watch<T = any>(whatToWatch: () => T, handler: (newVal: T, previousVal: T) => void): void
+
+export { ref, computed, watchEffect, watch };
